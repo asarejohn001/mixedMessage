1dc29b53d1345dd2000b71744bb6bd3d27ec211f
John Asare <137245223+asarejohn001@users.noreply.github.com>
2025-02-26 20:24:44 -0500

    create a funtion to store the messages

--- mixedMessage.js
+++ mixedMessage.js
@@ -10,4 +10,11 @@ inspirational message, or nonsensical jokes. To make your program truly random,
 the message that it outputs should be made up of at least three different pieces of data. 
 Take what you know of JavaScript syntax so far to build the program and customize it to your liking. */
 
-// function to generate a random number
+// function that stores the random messages
+const messages = {
+    message1: ['Hello', 'Hi', 'Hey', 'Good Morning', 'Good Afternoon', 'Good Evening'],
+    message2: ['John', 'Jane', 'Doe', 'Smith', 'Johnson', 'Williams'],
+    message3: ['How are you?', 'How is your day?', 'How is the weather?', 'How is the family?', 'How is the job?']
+}
+
+// function to pick random message from the messaages array
